524bdbc3443f835aa546d417f746339671f84604
Razor-eng <83156526+Razor-eng@users.noreply.github.com>
2023-03-19 00:04:48 +0530

    app build

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,7 @@ export default async function RootLayout({
     <html lang="en">
       <head />
       <body>
+        <Login />
         <SessionProvider session={session}>
           {!session ? (
             <Login />
